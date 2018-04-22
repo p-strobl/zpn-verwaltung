c7a9bb2cd3798bb895df970f2d853d7bee656503
@Home P. Strobl <p.strobl.hh@gmail.com>
2018-04-22 23:53:15 +0200

    work@ live Preview

---
+++ js/script-livePreview.js
@@ -0,0 +1,16 @@
+
+// let checkWindowHeight = function() {
+//
+//     let windowEvent = function(event) {
+//         let windowHeight = window.innerHeight;
+//         if(windowHeight <= 200) {
+//             console.log("Test");
+//         }
+//     };
+//
+//     window.addEventListener('resize', windowEvent, false);
+//
+// };
+//
+//
+// document.addEventListener('DOMContentLoaded', checkWindowHeight, false);
