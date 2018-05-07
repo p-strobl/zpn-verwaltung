c997d217de6dfc2ac83c8563d938478a4201ccf4
P. Strobl <p.strobl.hh@gmail.com>
2018-05-08 00:31:25 +0200

    bugfix 07.05.2018

--- js/script-livePreview.js
+++ js/script-livePreview.js
@@ -4,48 +4,6 @@ document.addEventListener('DOMContentLoaded', function () {
 
     var sse = new EventSource('../php/db-request-PreView-Items.php');
 
-    //
-    //Ajax call
-    // function updatePreview() {
-    //     const ajaxRequestPreview = $.ajax({
-    //         url: '../php/db-request-PreView-Items.php',
-    //         dataType: 'json'
-    //     });
-
-    //     ajaxRequestPreview.done((receivedPreviewItems) => {
-
-    //         const toFillPreviewItems = {
-    //             lfgbAnzahl: $('#daily-lfgb'),
-    //             textilAnzahl: $('#daily-Textil'),
-    //             zpnAnzahl: $('#daily-zpn'),
-    //             zpnPanelAnzahl: $('#panel-zpn'),
-    //             zpnKlaerfaelle: $('#panel-zpnKlaerfall'),
-    //             zpnMustereingang: $('#panel-zpnMusterEingang'),
-    //             zpnWagen: $('#panel-zpnWagen')
-    //         };
-
-    //         //
-    //         //Fügt die aus dem Ajax call erhaltenen Werte, wenn nicht schon im localStorage identisch in die entpsprechenden html Elemente ein
-    //         Object.entries(receivedPreviewItems).forEach(([receivedKey, receivedValue]) => {
-    //             Object.entries(toFillPreviewItems).forEach(([toFillKey, toFillValue]) => {
-    //                 if (receivedKey === toFillKey && receivedValue != toFillValue.html()) {
-    //                     let currentNumber = toFillValue.html();
-    //                     toFillValue.html(receivedValue);
-    //                     if (currentNumber <= receivedValue) {
-    //                         toFillValue.parent().effect('highlight', { color: '#ffb700' }, 4000);
-    //                     } else {
-    //                         toFillValue.parent().effect('highlight', { color: '#ff3300' }, 4000);
-    //                     }
-    //                 }
-    //             });
-    //         });
-    //     });
-
-    //     ajaxRequestPreview.fail((jqXHR, textStatus, errorThrown) => {
-    //         console.log(textStatus, errorThrown);
-    //     });
-    // }
-
     function updatePreview(receivedPreviewItems) {
         const toFillPreviewItems = {
             lfgbAnzahl: $('#daily-lfgb'),
@@ -77,9 +35,5 @@ document.addEventListener('DOMContentLoaded', function () {
     sse.onmessage = function (message) {
         updatePreview(JSON.parse(message.data));
     };
-
-    // setInterval(() => {
-    //     updatePreview();
-    // }, 5000);
 
 });
